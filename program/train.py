4c72f2ce48c5f1b580e4d9dc9d7ff78beed10b30
Jens-Jakob <KM44VW@student.aau.dk>
2025-04-16 15:49:25 +0200

    init train.py

--- program/train.py
+++ program/train.py
@@ -1,32 +1,29 @@
-import models.yolo
-import models.MoCo
-from models.mlp import MLP
-
-
-
-def train():
-    image = "path_to_random_img"
-
-    feature_vector = MoCo(image)
-    print(f"MoCO_feature_vector:{feature_vector}")
-
-    expert_weights = MLP(feature_vector)
-    print(f"Expert_weights:{expert_weights}")
-
-    expert_loss = YOLO
-
-
-    final_loss = (expert_weights[0]*expert_loss[0]+expert_weights[1]*
-            expert_loss[1]+expert_weights[2]*expert_loss[2])
-
-    pass
-
-
-
-
+import os
+import glob
+from utils.dataset import Dataset
 
 def main():
-    print("Main things")
+    # Replace this path with your own images directory
+    image_directory = '/ceph/project/P4-concept-drift/final_yolo_data_format/YOLOv8-pt/Dataset/images/test'
+
+    # Collect all image filenames
+    filenames = []
+    for ext in ["*.jpg", "*.jpeg", "*.png", "*.bmp", "*.tif", "*.tiff", "*.webp"]:
+        filenames.extend(glob.glob(os.path.join(image_directory, ext)))
+
+    # Instantiate the Dataset
+    input_size = 640
+    params = {}  # pass any additional parameters you need
+    augment = False  # set to True if you want to apply augmentation
+    ds = Dataset(filenames, input_size=input_size, params=params, augment=augment)
+
+    # Print the number of images
+    print(f"Number of images: {len(ds)}")
+
+    # Print some of the file paths
+    print("Here are a few file paths from the dataset:")
+    for i in range(min(5, len(ds))):  # show up to 5
+        print(ds.filenames[i])
 
 if __name__ == "__main__":
     main()
